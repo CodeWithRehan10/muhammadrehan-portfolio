1604e9a2648ab4862532f98a4e7174d386ecd14c
Muhammad Rehan <m.rehan2857@gmail.com>
2025-07-19 18:57:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-# config.py
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'm.rehan2857@gmail.com'        # 👈 apna Gmail
-MAIL_PASSWORD = 'junb obff vqci srxz'      # 👈 App password
-MAIL_DEFAULT_SENDER = 'your_email@gmail.com'
+
